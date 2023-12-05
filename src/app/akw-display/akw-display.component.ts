84e505b252e6778d782c63954d1dfac53a12a43c
Jochen Wißmann <jochen.wissmann@gmail.com>
2023-12-05 11:11:09 +0100

    akw-display.component.ts * Updated the 'akw-display'-component for variable icon size so that fractions (floating numbers) can also be displayed * Added logic to dynamically update the width of icons in the AKW display based on the total number of AKWs.
    akw-display.component.html
    * will now iterate over all cells in the grid an determine for each cell the width and CSS-class
    
    akw-display.component.css
    * The width and CSS class of each icon are now determined by a new calculateIconWidthAndCSSName() function. The partial filling of AKWs is visually improved by making the width of the icons adjustable, and renaming the CSS class ".partially-filled" for better readability.

--- src/app/akw-display/akw-display.component.ts
+++ src/app/akw-display/akw-display.component.ts
@@ -10,9 +10,35 @@ import {JsonPipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
   styleUrls: ['./akw-display.component.css']
 })
 export class AkwDisplayComponent {
-  @Input() numberOfAkws: number = 0;
+  @Input() set numberOfAkws(value: number) {
+
+    this.totalNumberOfAkws = value;
+  }
 
   // Create an array with 100 elements for the 10x10 grid
   akwArray = new Array(100).fill(0);
+  totalNumberOfAkws: number = 0;
+  filledIcons: number = 0;
 
+  /**
+   * Calculates the width and CSS name for an icon based on the given index.
+   *
+   * @param {number} index - The index of the icon.
+   * @return {Object} - An object containing the calculated width and CSS name.
+   *         - calculatedWidth: The width of the icon.
+   *         - calculatedCssName: The CSS name of the icon.
+   */
+  calculateIconWidthAndCSSName(index: number): { calculatedWidth: number, calculatedCssName: string } {
+    const MAX_CELL_WIDTH = 30;
+    const EMPTY_CELL_CSS = "akw-icon";
+    const FILLED_CELL_CSS = "partially-filled";
+    const value = this.totalNumberOfAkws - index;
+
+    let calculatedWidth: number = (value < 1 && value > 0 ) ? (value % 1) * MAX_CELL_WIDTH : MAX_CELL_WIDTH;
+    let calculatedCssName: string = (value > 0 ) ? FILLED_CELL_CSS : EMPTY_CELL_CSS;
+
+    console.log("index: ", index, "Width: ", calculatedWidth, "CSS: ", calculatedCssName);
+
+    return {calculatedWidth, calculatedCssName};
+  }
 }
